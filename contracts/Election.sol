ee099dc438a9d676785db42a686af32e6e41a2ef
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-25 17:49:03 +0000

    store a candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,17 @@ pragma solidity >=0.5.16;
 
 // declaring our contract 
 contract Election {
-    //store candidate
-    //Read condidate
-    string public candidate;
-    //constructor 
+    //Model a candidate 
+    struct Candidate {
+        uint id; 
+        string name; ;
+        uint voteCount;
+    }
+
+    //store candidates
+    
+    //fetch candidates 
+
     constructor() public{
         candidate = "Candidate 1";
     }
